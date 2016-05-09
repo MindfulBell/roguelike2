e2717f809cfaf7ddf99b12c15704410aca9238d9
Tim Bell <tim.bell02@gmail.com>
2016-05-09 16:30:48 -0700

    adding url-loader for jpgs/pngs

--- webpack.config.js
+++ webpack.config.js
@@ -15,11 +15,12 @@ var config = {
     loaders : [
       { test: /\.scss$/, loaders: ["style", "css", "sass"]},
       { test: /\.jsx?$/, exclude: /(node_modules|bower_components)/, loader: 'babel' },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader!resolve-url' },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
       { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
+      { test: /\.(jpg|png)$/, loader: "url-loader?limit=10000" }
     ]
   },
   plugins: [
